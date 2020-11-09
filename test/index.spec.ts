d570be003a52c72e452c6c5ac0aa7ef68ded9f3d
RafaelGSS <rafael.nunu@hotmail.com>
2020-11-09 18:38:54 -0300

    tests: add spec test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,5 @@
-import { OpenAPMDecorator } from '../'
+import 'open-apm';
+import { OpenAPMDecorator } from '..';
 
 describe('OpenAPMDecorator', () => {
   describe('no args', () => {
@@ -9,7 +10,7 @@ describe('OpenAPMDecorator', () => {
       // eslint-disable-next-line no-new-wrappers
       descriptor = { value: () => new Number(1) };
       decorate = OpenAPMDecorator({});
-      decorate({}, 'example key', descriptor)
+      decorate({}, 'example key', descriptor);
     });
 
     it('should return function', () => {
@@ -18,7 +19,28 @@ describe('OpenAPMDecorator', () => {
 
     it('should replace the descriptor.value with agent.wrap', () => {
       expect(descriptor.value).toBeInstanceOf(Function);
-      expect(descriptor.value()).toBeInstanceOf(Number);
+      expect(descriptor.value).toThrowError('The current context not exists');
+    });
+  });
+
+  describe('with async', () => {
+    let descriptor: any;
+    let decorate: any;
+
+    beforeEach(() => {
+      // eslint-disable-next-line no-new-wrappers
+      descriptor = { value: async () => new Number(1) };
+      decorate = OpenAPMDecorator({ isAsync: true });
+      decorate({}, 'example key', descriptor);
+    });
+
+    it('should return function', () => {
+      expect(decorate).toBeInstanceOf(Function);
+    });
+
+    it('should replace the descriptor.value with agent.wrap', async () => {
+      expect(descriptor.value).toBeInstanceOf(Function);
+      expect(descriptor.value).toThrowError('The current context not exists');
     });
   });
 });
